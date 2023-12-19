9030286bbec014b7cd0673442458c50736fdbf13
Carmelo Prestipino <carmelo.prestipino@gmail.com>
2023-12-19 09:42:38 +0100

    Python_course

---
+++ python_introduction/Torino.py
@@ -0,0 +1,12 @@
+club = 'Torino F.C.'
+
+song = 'Forza Toro'
+
+nemesis = 'Juventus'
+championchip= [1927, 1942, 1945, 1946, 1947, 1948, 1975]
+
+def team(player):
+    print(f'player = {player}')
+    
+class FCteam(object):
+    pass
